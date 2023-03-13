531cc0fb4d45f72cf4a1c07dcbee3e634c673581
a0919912233 <126413106+a0919912233@users.noreply.github.com>
2023-03-13 15:57:46 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LengthCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM; //宣告一個double變數，變數名稱叫douCM
+
+            douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            //將douCM的數值除以100，也就是從公分轉換成公尺
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
+        }
+    }
+}
